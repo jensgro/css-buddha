d5fa31e014c6402fbb6577803bdf0cb9036761c3
Jens Grochtdreis <jens@grochtdreis.de>
2021-02-18 23:09:57 +0100

    Bilder eingefügt und Card integriert - auch für Code

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
 
+  // Copy `images/` to `_site/images`
+  eleventyConfig.addPassthroughCopy("src/images");
+  // eleventyConfig.addPassthroughCopy("src/fonts");
+
   eleventyConfig.addPlugin(syntaxHighlight);
   return {
     dir: {
