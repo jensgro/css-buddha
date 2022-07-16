f4c3669a2b0d44c4d8694b83e20c0728ddbbf979
Jens Grochtdreis <jens@grochtdreis.de>
2022-07-16 19:38:47 +0200

    Alles aktualisiert, überarbeitet und neu verortet

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,69 @@
+const fs = require("fs");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/sass/");
+// filters
+const htmlDateString = require("./src/_11ty/filters/date.js").htmlDateString;
+const head = require("./src/_11ty/filters/head.js");
 
-  // Copy `images/` to `_site/images`
-  eleventyConfig.addPassthroughCopy("src/images");
-  // eleventyConfig.addPassthroughCopy("src/fonts");
+// collections
+const infosDescending = require("./src/_11ty/collections/infosDescending.js");
+const tagList = require("./src/_11ty/collections/tagList.js");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/assets/scss/");
+
+  eleventyConfig.addFilter("htmlDateString", htmlDateString);
+  eleventyConfig.addFilter("head", head);
+
+  eleventyConfig.addCollection("infosDescending", infosDescending);
+  eleventyConfig.addCollection("tagList", tagList);
+
+  // keine Ahnung, was das tut :-)
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addPassthroughCopy({"./src/assets/img": "/img"});
+  eleventyConfig.addPassthroughCopy("css");
+  // eleventyConfig.addPassthroughCopy({"./src/assets/scss/fonts": "css/fonts"});
+  eleventyConfig.addPassthroughCopy({"./src/static/":"/"});
 
   eleventyConfig.addPlugin(syntaxHighlight);
+  
+  /* Markdown Overrides */
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  });
+
+  // Watch CSS files for changes
+  eleventyConfig.setBrowserSyncConfig({
+    files: './_site/css/**/*.css'
+  });
+
   return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
     dir: {
       input: "src",
-      output: "public",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+      data: "_data",
+      output: "_site",
     },
   };
 };
--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # CSS-Buddha
-
-Basierend auf dem "11ty Sass Skeleton" von [@5t3ph](https://twitter.com/5t3ph).
-
-
+Artikel rund ums Frontend, hauptsächlich CSS.
+Ursprünglich begann dieses Projekt auf der Basis des "11ty Sass Skeleton" von [@5t3ph](https://twitter.com/5t3ph). Aber dann fand ich die Methodik von [Jérôme Coupé](https://github.com/jeromecoupe) viel interessanter und habe das Repo aktualisiert und [nach seinen Ideen](https://www.webstoemp.com/blog/eleventy-projects-structure/) umgebaut.
 
 
-## Development Scripts
+
+## Die Skripte
 
 **`npm start`**
 
-> Run 11ty with hot reload at localhost:8080, including reload based on Sass changes
+11ty wird mit Watchern auf Sass und Änderungen an HTML und den Templates auf localhost:8080 gestartet.
 
 **`npm run build`**
 
-> Production build includes minified, autoprefixed CSS
+Das Ergebnis dieses Tasks kann live gehen. Bei einem Hoster wie Netlify sollte dieser Task getriggert werden.
 
-Use this as the "Publish command" if needed by hosting such as Netlify.
+**`npm run clean`**
+
+Die statische Ausgabe des Projektes wird gelöscht.
